cce895a850fecd46cacd03f797731bd032a07413
SpeakerLive <62421000+SpeakerLive@users.noreply.github.com>
2025-05-17 11:35:28 +0200

    Initial commit
    Initial Commit

---
+++ Source/LevelDesign_Test/LevelDesign_Test.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LevelDesign_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LevelDesign_Test, "LevelDesign_Test" );
